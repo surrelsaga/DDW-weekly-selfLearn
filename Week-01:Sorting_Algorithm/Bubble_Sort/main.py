c450b49c0dab02b3b98849677adba6ef0879f2e6
Daniel Nguyen <233656241+DucAnh0106@users.noreply.github.com>
2026-05-19 11:28:31 +0800

    week-01: add pseudocode and implement the algo for bubble sort v1

--- Week-01:Sorting_Algorithm/Bubble_Sort/main.py
+++ Week-01:Sorting_Algorithm/Bubble_Sort/main.py
@@ -1,7 +1,37 @@
-# Pseudocode
+# My Pseudocode
 
-# inner iterations: for loop through the list and compare list[i] with list[i + 1]
-# then swap list[i], list[i + 1] = list[i+1], list[i]
+# Declare the list here 
+# outer iterations (n - 1 iterations): for loop for n - 1 times, each time we execute the inner iteration
+# inner iterations: for loop through the list, compare list[i] vs list[i + 1] and do a swap if left > right
 
 
-# 
+# Course pseudocode
+
+# Bubble sort
+# Version: 1
+# Input: array
+# Output: None, sort in place
+# Steps:
+# 1. get n = array length
+# 2. For outer_index from 1 to n -1, do:
+#     2.1 For inner_index from 1 to n -1, do:
+#         2.1.1 declare firstNumber = array[inner_index - 1]
+#         2.1.2 declare secondNumber = array[inner_index]
+#         2.1.3 if first_number > second_number, do:
+            #  2.1.3.1 swap(firstNumber, secondNumber)
+            
+# Code here
+arr = [16, 14, 10, 8, 7, 8, 3, 2, 4, 1]
+print(arr)
+
+n = len(arr)
+for outer_index in range(1, n-1):
+    for inner_index in range(1, n-1):
+        firstNumber = arr[inner_index - 1]
+        secondNumber = arr[inner_index]
+        
+        if( firstNumber > secondNumber ):
+            # swap
+            arr[inner_index - 1], arr[inner_index] = arr[inner_index], arr[inner_index - 1]
+            
+print(arr)
