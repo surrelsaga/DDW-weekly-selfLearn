03e32d45bf6f8a62b04639ead0de92f518dbf5ed
Daniel Nguyen <233656241+DucAnh0106@users.noreply.github.com>
2026-06-01 12:50:51 +0800

    week03(towerOfHanoi): add all detailed comments on steps in the algo implementation

---
+++ Week-03:Divide&Conquer/Recursion/towerOfHanoi.py
@@ -0,0 +1,46 @@
+# PCDIT
+
+# Problem statements: (check image in the doc)
+# Give n disks and 3 towers (all n disks are at the first tower called 'source')
+# so we have source, destination and auxiliary (tower to help with the moving in the algo)
+
+# Cases: (visualization)
+
+# DESIGN OF ALGO: 
+# basic understanding: In each iteration, we take all the top disks (except for
+# ONLY THE BOTTOM ONE) -> we move to the auxiliary
+# then, the bottom one is moved to the destination tower.
+
+# in the subsequent iteration, the empty tower (the old source tower) become
+# the new auxiliary tower.
+# So the pattern that happens in every recursion is
+# "TOP DISKS ARE MOVED TO AUXILIARY, BOTTOM IS MOVED TO DESTINATION"
+
+# Substeps in a step to move all top disks to auxiliary (we must keep the ascending order downward)
+# reason?: is to have a pattern so we can use recursion
+# Substeps: first disk always move to destination, until we only left with one disk
+# above the actual bottom disk, then we move that disk to auxiliary and move all the
+# previous top disks back to auxiliary
+# These are all the substeps to form one step of move the first n - 1 disks from source to auxiliary
+
+# ONE IMPORTANT NOTES: the disk stack is smallest on top, largest at bottom
+# they are labelled with numbers from 1 to n.
+# the smaller the disk number is, the smaller the its size is
+
+# PSEUDOCODE
+# Input:
+#   - n, number of disks
+#   - source tower
+#   - destination tower
+#   - auxiliary tower
+# Output:
+#   - sequence of steps to move n disks from source to destination tower using auxiliary tower
+# Steps:
+# 1. if n is 1 disk:
+#     1.1 Move the one disk from source to destination tower
+# 2. otherwise, if n is greater than 1:
+#     2.1 Move the first n - 1 disks from source to auxiliary tower
+#     2.2 Move the last disk n from source to destination tower
+#     2.3 Move the first n - 1 disks from the auxiliary tower to the destination tower
+
+        
